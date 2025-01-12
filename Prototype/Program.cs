ceb22c6265b5321446b7bf40a1132c637af058db
ivanchikk <vanyaklochkoff@ukr.net>
2025-01-12 15:44:44 +0200

    Implemented prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Prototype;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var p1 = new ConcretePrototype1("string")
+        {
+            PublicStringProperty1 = "string property1",
+            PublicStringProperty2 = "string property2",
+            PublicStringProperty3 = "string property3"
+        };
+
+        var c1 = p1.Clone() as ConcretePrototype1;
+        Console.WriteLine(p1);
+        Console.WriteLine(c1);
+
+        Console.WriteLine();
+
+        var p2 = new ConcretePrototype2(12)
+        {
+            PublicIntProperty1 = 1,
+            PublicIntProperty2 = 2,
+            PublicIntProperty3 = 3
+        };
+
+        var c2 = p2.Clone() as ConcretePrototype2;
+        Console.WriteLine(p2);
+        Console.WriteLine(c2);
+    }
+}
