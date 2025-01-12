1ca20e66c6a14477465bc206e2caca4942470c32
ivanchikk <vanyaklochkoff@ukr.net>
2025-01-12 13:47:01 +0200

    Implemented singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Singleton;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Main\n");
+
+        var s11 = Singleton1.Instance;
+        var s12 = Singleton1.Instance;
+        Console.WriteLine($"s11 = s12: {s11 == s12}");
+
+        var s21 = Singleton2.Instance;
+        var s22 = Singleton2.Instance;
+        Console.WriteLine($"s21 = s22: {s21 == s22}");
+
+        var s31 = Singleton3.Instance;
+        var s32 = Singleton3.Instance;
+        Console.WriteLine($"s31 = s32: {s31 == s32}");
+    }
+}
