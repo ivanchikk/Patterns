ffa14854a7935f8696df0c7d1e086682eb05763b
ivanchikk <vanyaklochkoff@ukr.net>
2025-01-13 19:32:48 +0200

    Implemented flyweight

---
+++ Flyweight/Product.cs
@@ -0,0 +1,10 @@
+namespace Flyweight;
+
+public class Product
+{
+    public string? RepeatingState1 { get; set; }
+    public string? RepeatingState2 { get; set; }
+    public string? UniqueState1 { get; set; }
+    public string? UniqueState2 { get; set; }
+    public string? UniqueState3 { get; set; }
+}
