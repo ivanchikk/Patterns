d074cb59c72957e2275e0838591e15e9f483cb57
ivanchikk <vanyaklochkoff@ukr.net>
2025-01-14 12:59:19 +0200

    Implemented strategy

---
+++ Strategy/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Strategy;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        var context = new Context(new ConcreteStrategy1());
+        context.Input = "Input";
+
+        Console.WriteLine(context.Operation());
+
+        context.Strategy = new ConcreteStrategy2();
+
+        Console.WriteLine(context.Operation());
+    }
+}
